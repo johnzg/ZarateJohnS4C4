47cdc58733a8103237037e68db3c90d829706231
johnzg <js.zarate@uniandes.edu.co>
2019-06-28 17:17:28 +0000

    producto

---
+++ ZarateJohnS4C4/producto.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    float b;
+    a=10;
+    b=5.7;
+    cout<<"el producto de 10 por 5.7 es: ";
+    cout<<a*b<<endl;
+    return 0;
+}
+  
